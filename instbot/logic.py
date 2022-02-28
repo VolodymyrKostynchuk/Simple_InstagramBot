02e6f4bfa05dc40244b36c2195990364f78576b0
VolodymyrKostynchuk <99145753+VolodymyrKostynchuk@users.noreply.github.com>
2022-02-28 07:17:22 +0200

    Update logic.py

--- instbot/logic.py
+++ instbot/logic.py
@@ -21,12 +21,15 @@ def autorization(login, password):
 	password_input.send_keys(Keys.ENTER)
 	time.sleep(3)
 
+	
 def close_browzer():
 	browzer.quit()
 
+	
 def get_url_photo():
 	hrefs = browzer.find_elements_by_tag_name('a')
 	return [item.get_attribute('href') for item in hrefs if '/p/' in item.get_attribute('href')]
+
 
 def las(post_url, by_hachtag=False, by_nickname=False):
 	# by hachtag needed because if we run "lash" we will feel different users every time
@@ -44,9 +47,11 @@ def las(post_url, by_hachtag=False, by_nickname=False):
 			time.sleep(1)
 	time.sleep(3)
 
+	
 def get_count_post(count_post_on_page_xpath):
 	count_post = browzer.find_element_by_xpath(count_post_on_page_xpath).text
 	return float(count_post.replace(' ', ''))
+
 
 def count_scroll(count_post):
 	# there is a restriction because more than 1400 posts cannot be liked on Instagram
@@ -56,17 +61,20 @@ def count_scroll(count_post):
 	scrolls = math.ceil(scrolls) 
 	return scrolls
 
+
 def scroll(scrolls):
 	for scroll in range(1, scrolls):
 		browzer.execute_script('window.scrollTo(0, document.body.scrollHeight);') 
 		time.sleep(2)
 	time.sleep(3)
 
+	
 def add_url_photo(post_url):
 	with open('url_post.txt', 'a') as file:
 		for url in post_url:
 			file.write(url + '\n')
 
+			
 def get_url_on_clear():
 	with open('url_post.txt', 'r+') as file:
 		url_on_clear = []
@@ -75,11 +83,13 @@ def get_url_on_clear():
 		file.truncate(0)
 	return url_on_clear
 
+
 def clear_las(url_on_clear):
 	for url in url_on_clear:
 		browzer.get(url)
 		time.sleep(1)
 		unsuscribe_user()
+	
 	
 def unsuscribe_user():
 	like_button = browzer.find_element_by_xpath('/html/body/div[1]/section/main/div/div/article/div/div[2]/div/div[2]/section[1]/span[1]/button').click()
@@ -92,26 +102,32 @@ def unsuscribe_user():
 		unsubscribe_button = browzer.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[1]').click()
 	time.sleep(1)
 
+	
 def media_src(media_xpath):
 	media_src = browzer.find_element_by_xpath(media_xpath).get_attribute('src')
 	return media_src
+
 
 def get_first_xpath(tagname, attribute):
 	imgs = browzer.find_elements_by_tag_name(tagname)
 	img_src = [item.get_attribute(attribute) for item in imgs ]
 	return img_src[0]
 
+
 def get_namefile():
 	namefile = browzer.current_url.split('/')[-2]
 	return namefile
+
 
 def get_nameforder(xpath_to_nickname):
 	nameforder = browzer.find_element_by_xpath(xpath_to_nickname).text
 	return nameforder
 
+
 def xpath_forder(nameforder, format_media):
 	xpath_forder = f'media\\{nameforder}\\{format_media}'
 	return xpath_forder
+
 
 def create_forder(nameforder):
 	forders = ('img', 'stories', 'video', 'avatar')
@@ -121,6 +137,7 @@ def create_forder(nameforder):
 		for forder in forders:
 			os.makedirs(f'{xpath_forder}\\{forder}')
 
+			
 def download_img(img_src, nameforder, namefile, format_media):
 	create_forder(nameforder)
 	forder = xpath_forder(nameforder, format_media)
@@ -128,6 +145,7 @@ def download_img(img_src, nameforder, namefile, format_media):
 	with open(f'{forder}\\{namefile}.png', 'wb') as file:
 		file.write(get_img.content)
 
+		
 def download_video(media_src, nameforder, namefile, format_media):
 	create_forder(nameforder)
 	forder = xpath_forder(nameforder, format_media)
